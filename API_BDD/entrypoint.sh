fb302c74601e7503e690636fc70ad9aa2121eaa6
KossiTHEDON <81193993+KossiJunior@users.noreply.github.com>
2022-01-19 23:33:04 +0100

    Create entrypoint.sh

---
+++ API_BDD/entrypoint.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+# Configuration de l'interface réseau
+ip a flush eth0
+ip a add 192.168.1.7/24 dev eth0
+ip r add default via 192.168.1.1 dev eth0
+# Lancement de l'application et service actif du container
+python ./app.py
