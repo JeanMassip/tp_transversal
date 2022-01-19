c9ec24cf893dd8f760964eab4cfd6720b6d6eec6
KossiTHEDON <81193993+KossiJunior@users.noreply.github.com>
2022-01-19 22:54:57 +0100

    Update ip.sh

--- FileTampon/ip.sh
+++ FileTampon/ip.sh
@@ -1,8 +1,9 @@
 #! /bin/sh
-
+# Configuration de l'interface réseau
 ip a flush eth0
 ip a add 192.168.1.78/24 dev eth0
 ip r add default via 192.168.1.222 dev eth0
+# Lancement de l'application et service actif du container
 python ./app.py
 
 
