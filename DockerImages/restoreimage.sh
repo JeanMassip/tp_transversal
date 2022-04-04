de17006ffb77b827b8e0f46560f01d6d6662c69f
JeanMassip <29867851+JeanMassip@users.noreply.github.com>
2022-04-04 13:47:25 +0200

    Delete DockerImages directory

--- DockerImages/restoreimage.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Programme qui va restorer une image docker qui a été
-# sauvegardée au fomat .tar
-# Argument à fournir : nom du fichier .tar de l'image
-# à restorer
-# Utilisation restoreimage.sh <NomImageARestorer.tar>
-# ! A lancer avec les droits sudo.
-
-sudo docker load < $1
